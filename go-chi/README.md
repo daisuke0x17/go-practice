883375ea9155546617ddcb2fd64c15a0bfc23f64
daisuke23 <daisuke0x17@gmail.com>
2023-11-05 14:37:17 +0900

    feat: zenn tutorial

--- go-chi/README.md
+++ go-chi/README.md
@@ -1,6 +1,5 @@
-## Notes
-
-
 ## 資料
 - 公式
     - https://go-chi.io/#/README
+- 参考
+    - https://zenn.dev/shibata/articles/25fba3adee2a44
--- go-chi/main.go
+++ go-chi/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -10,8 +11,21 @@ import (
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
+		fmt.Fprint(w, "Hello World!")
 	})
+
+	// Delete リクエストの場合
+	r.Delete("/delete", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "This is Delete request")
+	})
+
+	// パスパラメータを受け取る場合
+	r.Get("/{userId}/detail", func(w http.ResponseWriter, r *http.Request) {
+		userId := chi.URLParam(r, "userId")
+		fmt.Fprintf(w, "userId is %s", userId)
+	})
+
 	http.ListenAndServe(":3000", r)
 }
