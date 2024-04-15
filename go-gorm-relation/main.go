7ad3aeb70249260cb9cbd28aa8c299fc6bc3ce86
daisuke0x17 <daisuke0x17@gmail.com>
2024-04-15 23:21:36 +0900

    feat: gorm の belongs to 動作確認

---
+++ go-gorm-relation/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID        int `gorm:"primaryKey;autoIncrement"`
+	Name      string
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime"`
+}
+
+type Tweet struct {
+	ID        int `gorm:"primaryKey;autoIncrement"`
+	UserID    int
+	User      User
+	Text      string
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime"`
+}
+
+func main() {
+	dsn := "host=localhost user=postgres password=postgres dbname=sample_db port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&User{})
+	db.AutoMigrate(&Tweet{})
+	fmt.Println("migrated")
+
+	var count int64
+	db.Model(&User{}).Count(&count)
+	if count == 0 {
+		db.Create(&User{Name: "user01"})
+		db.Create(&User{Name: "user02"})
+		db.Create(&User{Name: "user03"})
+	}
+
+	db.Model(&Tweet{}).Count(&count)
+	if count == 0 {
+		db.Create(&Tweet{UserID: 1, Text: "tweet01"})
+		db.Create(&Tweet{UserID: 1, Text: "tweet02"})
+		db.Create(&Tweet{UserID: 1, Text: "tweet03"})
+	}
+
+	var tweet []Tweet
+	db.Model(&tweet).Preload("User").Find(&tweet)
+	json, err := json.MarshalIndent(tweet, "", "  ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(json))
+}
